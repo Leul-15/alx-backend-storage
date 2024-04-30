dcd3a86ec75e50850291c46929193feab03f6596
Leul <Leulassefa255@gmail.com>
2024-04-30 03:28:07 -0700

    NoSQL

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Returns all students """
+
+
+def top_students(mongo_collection):
+    """ Returns all students """
+    top_student = mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ])
+
+    return top_student
